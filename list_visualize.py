db8efa0f6da43320de61997948db8d932fa0d8c7
unknown <ejshim@kist.re.kr>
2020-04-20 15:46:53 +0900

    list visualize.py

--- list_visualize.py
+++ list_visualize.py
@@ -5,63 +5,80 @@ import random
 import numpy as np
 
 
-db = pymongo.MongoClient().maxilafacial
-data_id_list = db.segmentationTrainProcessed_dev_train_ver3.find({}).distinct('_id')
-data_id = data_id_list[1]
-element = db.segmentationTrainProcessed_dev_train_ver3.find_one({"_id" : data_id})
-data = element['input']
-gt = element['gt']
 
-Points = vtk.vtkPoints()
-Vertices = vtk.vtkCellArray()
-
-for idx in range(len(data)) :
-        
-    id = Points.InsertNextPoint(data[idx])
-    Vertices.InsertNextCell(1)
-    Vertices.InsertCellPoint(id)
-
-polydata = vtk.vtkPolyData()
-polydata.SetPoints(Points)
-polydata.SetVerts(Vertices)
-polydata.Modified()
-
-
-#Assign gt
-pointCloud = polydata.GetPoints()
-num_points = pointCloud.GetNumberOfPoints()
-
-label = vtk.vtkFloatArray()
-for idx in range(num_points):
-    label.InsertNextValue(gt[idx])
-
-polydata.GetPointData().SetScalars(label)
-polydata.GetPointData().Modified()
-
-
-#Visualize
-mapper = vtk.vtkPolyDataMapper()
-mapper.SetInputData(polydata)
-mapper.SetColorModeToMapScalars()
-# mapper.GetLookupTable().SetUseBelowRangeColor(True)
-# mapper.GetLookupTable().SetBelowRangeColor(1.0, 1.0, 1.0, 1.0)
-mapper.SetScalarRange([0.0, 15.0])
-
-actor = vtk.vtkActor()
-actor.SetMapper(mapper)
-actor.GetProperty().SetPointSize(2)
-
+#Renderer
 renderer = vtk.vtkRenderer()
-renderer.AddActor(actor)
-renderer.SetBackground(.1, .2, .3)
+renderer.SetBackground(0.1, 0.2, 0.4) # R G B 
 
-renderWindow = vtk.vtkRenderWindow()
-renderWindow.AddRenderer(renderer)
-renderWindow.SetSize(1000, 1000)
-renderWindowInteractor = vtk.vtkRenderWindowInteractor()
-renderWindowInteractor.SetRenderWindow(renderWindow)
+#Render window
+renWin = vtk.vtkRenderWindow()
+renWin.AddRenderer(renderer)
+renWin.SetSize(1000, 1000)
 
-renderWindow.Render()
-renderWindowInteractor.Start()
+#Render window interactor
+iren = vtk.vtkRenderWindowInteractor()
+interactorStyle = vtk.vtkInteractorStyleTrackballCamera()
+iren.SetInteractorStyle(interactorStyle)
+iren.SetRenderWindow(renWin)
+
+
+
+def make_actor(polydata):
+    
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputData(polydata)
+    mapper.SetColorModeToMapScalars()
+    # mapper.GetLookupTable().SetUseBelowRangeColor(True)
+    # mapper.GetLookupTable().SetBelowRangeColor(1.0, 1.0, 1.0, 1.0)
+    mapper.SetScalarRange([1.0, 15.0])
+
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+    actor.GetProperty().SetPointSize(2)
+    # actor.GetProperty().SetColor(colors.GetColor3d("Yellow"))
+
+    return actor
+
+    
+
+if __name__=="__main__":
+
+
+    db = pymongo.MongoClient().maxilafacial
+    data_id_list = db.segmentationTrainProcessed_dev_train_ver3.find({}).distinct('_id')
+    data_id = data_id_list[1]
+    element = db.segmentationTrainProcessed_dev_train_ver3.find_one({"_id" : data_id})
+    data = element['input']
+    gt = element['gt']
+
+    Points = vtk.vtkPoints()
+    Vertices = vtk.vtkCellArray()
+    Labels = vtk.vtkFloatArray()
+
+    for idx in range(len(data)) :
+            
+        id = Points.InsertNextPoint(data[idx])
+        Vertices.InsertNextCell(1)
+        Vertices.InsertCellPoint(id)
+        #Assign gt
+        Labels.InsertNextValue(gt[idx])
+
+
+    polydata = vtk.vtkPolyData()
+    polydata.SetPoints(Points)
+    polydata.SetVerts(Vertices)
+    polydata.Modified()
+
+    polydata.GetPointData().SetScalars(Labels)
+    polydata.GetPointData().Modified()
+
+
+    #Visualize
+    actor = make_actor(polydata)
+    renderer.AddActor(actor)
+    renderer.ResetCamera()
+    renWin.Render()
+    iren.Start()
+
 
 
